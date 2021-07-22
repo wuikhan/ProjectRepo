b5c5825f2aff6d8941dd3e83a14ed1a718db0c81
Anya Flores <caramelkaya@gmail.com>
2021-07-22 15:09:02 -0400

    some changes

--- src/test/java/steps/OpportunityAnyaTest.java
+++ src/test/java/steps/OpportunityAnyaTest.java
@@ -11,50 +11,34 @@ import cucumber.api.java.en.Then;
 
 public class OpportunityAnyaTest extends BaseClass {
 
-	/*
-	 * @Then("^I enter \"([^\"]*)\" in the \"([^\"]*)\" field$") public void
-	 * i_enter_in_the_field(String textToEnter, String fieldName) {
-	 * 
-	 * if(fieldName.equalsIgnoreCase("Opportunity Name")) {
-	 * driver.findElement(By.id("opp3")).sendKeys(textToEnter); } }
-	 */
 
-	@Then("^I type \"([^\"]*)\" in the \"([^\"]*)\" field for Opportunity object$")
-	public void i_type_in_the_field_for_Opportunity_object(String textToEnter, String fieldName) {
-		if (fieldName.equalsIgnoreCase("Opportunity Name")) {
-			driver.findElement(By.id("opp3")).sendKeys(textToEnter);
-		}
-	}
-
-	@Then("^I select \"([^\"]*)\" in the \"([^\"]*)\" calendar field$")
-	public void i_select_in_the_calendar_field(String arg1, String arg2) {
-		driver.findElement(By.name("opp9")).click();
-
-		WebElement month = driver.findElement(By.id("calMonthPicker"));
-		Select selectMonth = new Select(month);
-		selectMonth.selectByVisibleText("February");
-
-		WebElement year = driver.findElement(By.id("calYearPicker"));
-		Select selectYear = new Select(year);
-		selectYear.selectByVisibleText("2022");
-
-		List<WebElement> dates = driver.findElements(By.xpath("//tr[@class='calRow']/td"));
-
-		for (WebElement date : dates) {
-			String value = date.getText();
-
-			if (value.contains("22")) {
-				date.click();
-				break;
-			}
-		}
-	}
-
-	@Then("^I select \"([^\"]*)\" for the \"([^\"]*)\" dropdown field$")
-	public void i_select_for_the_dropdown_field(String value, String fieldName) {
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
-		WebElement dDown = driver.findElement(By.name(fieldName));
-		Select sel = new Select(dDown);
-		sel.selectByValue(value);
-	}
+@Then("^I select \"([^\"]*)\" in the \"([^\"]*)\" calendar field$")
+public void i_select_in_the_calendar_field(String arg1, String arg2) {
+  driver.findElement(By.name("opp9")).click();
+    
+	 WebElement month = driver.findElement(By.id("calMonthPicker")); 
+	 Select selectMonth = new Select(month); 
+	  selectMonth.selectByVisibleText("February");
+	 
+	  WebElement year = driver.findElement(By.id("calYearPicker")); 
+	 Select selectYear = new Select(year); 
+	 selectYear.selectByVisibleText("2022");
+	 
+	  List<WebElement> dates = driver.findElements(By.xpath("//tr[@class='calRow']/td"));
+	 
+	 for (WebElement date : dates) {
+	  String value = date.getText();
+	 
+	 if (value.contains("22")) { 
+	  date.click(); break;
+	   }}
+}
+@Then("^I select \"([^\"]*)\" for the \"([^\"]*)\" dropdown field$")
+public void i_select_for_the_dropdown_field(String value, String fieldName)
+{
+	driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+	WebElement dDown = driver.findElement(By.name(fieldName));
+   Select sel = new Select(dDown);
+   sel.selectByValue(value);
+}
 }
